9680769d5341168654788aa6dab398b46ea8e30d
jomazao <jose@upstart13.com>
2020-09-30 19:16:27 -0500

    (New) ViewPager added to the mission screen

--- app/src/main/java/com/upstart13/legba/fragment/MissionFragment.java
+++ app/src/main/java/com/upstart13/legba/fragment/MissionFragment.java
@@ -12,14 +12,13 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.widget.ImageButton;
 import android.widget.TextView;
-import android.widget.Toast;
 
+import com.makeramen.roundedimageview.RoundedImageView;
 import com.sothree.slidinguppanel.SlidingUpPanelLayout;
 import com.upstart13.legba.HostActivity;
 import com.upstart13.legba.R;
@@ -30,6 +29,7 @@ import com.upstart13.legba.databinding.FragmentMissionBinding;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import timber.log.Timber;
 
@@ -54,10 +54,17 @@ public class MissionFragment extends Fragment {
         updateToolbar();
         setHasOptionsMenu(true);
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_mission, container, false);
+        List<Channel> nonRadioChannels = mission.channels
+                .stream()
+                .filter(channel -> channel.type != Channel.ChannelType.RADIO)
+                .collect(Collectors.toList());
+
+        binding.missionViewPager.setAdapter(new ChannelSlidePageAdapter(this, nonRadioChannels));
+        binding.springDotsIndicator.setViewPager2(binding.missionViewPager);
 
         //Primary channel
 
-        Channel primaryChannel = mission.channels
+/*        Channel primaryChannel = mission.channels
                 .stream()
                 .filter(channel -> channel.type != null)
                 .filter(channel -> channel.type == Channel.ChannelType.PRIMARY)
@@ -97,7 +104,7 @@ public class MissionFragment extends Fragment {
                 binding.priorityChannel2Name.setText(priorityChannel2.name);
                 binding.priorityChannel2LinkImage.setOnClickListener(view -> goToChannelFragment(priorityChannel2));
             }
-        }
+        }*/
 
         //Sliding up layout
         binding.radioChannelsSlidingupLayout.addPanelSlideListener(new SlidingUpPanelLayout.PanelSlideListener() {
@@ -110,9 +117,9 @@ public class MissionFragment extends Fragment {
             public void onPanelStateChanged(View panel, SlidingUpPanelLayout.PanelState previousState, SlidingUpPanelLayout.PanelState newState) {
                 ImageButton toggleRadioChannelButton = binding.toggleRadioChannelButton;
                 float newRotation = toggleRadioChannelButton.getRotation() + 180F;
-                if(newState == SlidingUpPanelLayout.PanelState.EXPANDED){
+                if (newState == SlidingUpPanelLayout.PanelState.EXPANDED) {
                     toggleRadioChannelButton.animate().rotation(newRotation).setInterpolator(new AccelerateDecelerateInterpolator());
-                }else if(newState == SlidingUpPanelLayout.PanelState.COLLAPSED){
+                } else if (newState == SlidingUpPanelLayout.PanelState.COLLAPSED) {
                     toggleRadioChannelButton.animate().rotation(newRotation).setInterpolator(new AccelerateDecelerateInterpolator());
                 }
             }
@@ -147,10 +154,10 @@ public class MissionFragment extends Fragment {
         Timber.i("updateToolbar MissionFragment");
         requireActivity().findViewById(R.id.logo_image).setVisibility(View.VISIBLE);
         requireActivity().findViewById(R.id.toolbar_title_text).setVisibility(View.VISIBLE);
-        ((TextView)requireActivity().findViewById(R.id.toolbar_title_text)).setText(mission.name);
+        ((TextView) requireActivity().findViewById(R.id.toolbar_title_text)).setText(mission.name);
         requireActivity().findViewById(R.id.fragment_description).setVisibility(View.VISIBLE);
-        ((TextView)requireActivity().findViewById(R.id.fragment_description)).setText("Main Channels");
-        ((TextView)requireActivity().findViewById(R.id.fragment_description)).setTextColor(this.getResources().getColor(R.color.paleRed));
+        ((TextView) requireActivity().findViewById(R.id.fragment_description)).setText("Main Channels");
+        ((TextView) requireActivity().findViewById(R.id.fragment_description)).setTextColor(this.getResources().getColor(R.color.paleRed));
         Objects.requireNonNull(((HostActivity) requireActivity()).getSupportActionBar()).setHomeAsUpIndicator(R.drawable.ic_round_keyboard_arrow_left_24);
     }
 
@@ -158,5 +165,105 @@ public class MissionFragment extends Fragment {
     public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
         super.onCreateOptionsMenu(menu, inflater);
         inflater.inflate(R.menu.mission_fragment_menu, menu);
+    }
+
+    private class ChannelSlidePageAdapter extends RecyclerView.Adapter<ChannelSlidePageAdapter.ChannelViewHolder> {
+
+        private Fragment fragment;
+        private List<Channel> channels;
+
+        private int priorityIndicator = 1;
+        private boolean paleRed = true;
+        private boolean waterBlue = false;
+        private boolean orange = false;
+
+        public void setChannels(List channels) {
+            this.channels = channels;
+            notifyDataSetChanged();
+        }
+
+        public ChannelSlidePageAdapter(Fragment fragment, List channels) {
+            this.fragment = fragment;
+            this.channels = channels;
+        }
+
+        @NonNull
+        @Override
+        public ChannelViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            Timber.i("Creando page");
+            View itemView = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.channel_item, parent, false);
+            return new ChannelViewHolder(itemView);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ChannelViewHolder holder, int position) {
+            Timber.i("onBindViewHolder - ChannelSlidePageAdapter");
+            Channel currentChannel = channels.get(position);
+            holder.channelInfo.setOnClickListener(view -> NavHostFragment.findNavController(fragment)
+                    .navigate(MissionFragmentDirections.actionMissionFragmentToChannelFragment(currentChannel)));
+            holder.channelImage.setImageResource(getImageResource(currentChannel.image));
+            holder.channelName.setText(currentChannel.name);
+            holder.channelType.setText(getTypeString(currentChannel.type));
+
+            if (paleRed) {
+                paleRed = false;
+                waterBlue = true;
+            } else if (waterBlue) {
+                waterBlue = false;
+                orange = true;
+
+                holder.channelImage.setBorderColor(getWaterBlueColor());
+                holder.channelType.setTextColor(getWaterBlueColor());
+            } else if (orange) {
+                orange = false;
+                paleRed = true;
+
+                holder.channelImage.setBorderColor(getOrangeColor());
+                holder.channelType.setTextColor(getOrangeColor());
+            }
+        }
+
+        private int getWaterBlueColor() {
+            return fragment.getResources().getColor(R.color.waterBlue);
+        }
+
+        private int getOrangeColor() {
+            return fragment.getResources().getColor(R.color.orange);
+        }
+
+        private String getTypeString(Channel.ChannelType type) {
+            switch (type) {
+                case PRIMARY:
+                    return "Primary Channel";
+                case PRIORITY:
+                    return "Priority Channel " + priorityIndicator;
+                case RADIO:
+                    return "Radio Channel";
+                default:
+                    return "";
+            }
+        }
+
+        @Override
+        public int getItemCount() {
+            return channels.size();
+        }
+
+        class ChannelViewHolder extends RecyclerView.ViewHolder {
+
+            private View channelInfo;
+            private RoundedImageView channelImage;
+            private TextView channelName;
+            private TextView channelType;
+
+            public ChannelViewHolder(@NonNull View itemView) {
+                super(itemView);
+                channelInfo = itemView.findViewById(R.id.channel_info);
+                channelImage = itemView.findViewById(R.id.channel_image);
+                channelName = itemView.findViewById(R.id.channel_name_text);
+                channelType = itemView.findViewById(R.id.channel_type_text);
+            }
+        }
     }
 }
