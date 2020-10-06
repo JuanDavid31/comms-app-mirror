e13fd50bd28072e06576caf7ce8123f0091a210c
Jomazao <jomazao@gmail.com>
2020-10-06 16:58:13 -0500

    adding classes for receiving messages

---
+++ app/src/main/java/com/upstart13/legba/engage/SimpleMessageException.java
@@ -0,0 +1,14 @@
+//
+//  Copyright (c) 2019 Rally Tactical Systems, Inc.
+//  All rights reserved.
+//
+
+package com.upstart13.legba.engage;
+
+public class SimpleMessageException extends Exception
+{
+    public SimpleMessageException(String msg)
+    {
+        super(msg);
+    }
+}
