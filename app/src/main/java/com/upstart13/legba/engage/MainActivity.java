06b1fa58257af0cec2140df349e339c8ba13337b
Jomazao <jomazao@gmail.com>
2020-10-06 16:58:13 -0500

    adding classes for receiving messages

---
+++ app/src/main/java/com/upstart13/legba/engage/MainActivity.java
@@ -0,0 +1,23 @@
+//
+//  Copyright (c) 2019 Rally Tactical Systems, Inc.
+//  All rights reserved.
+//
+
+package com.upstart13.legba.engage;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.upstart13.legba.R;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.empty_layout);
+    }
+}
