aef385879b69db88bed8a4cd50d88935aba8bdc7
nicolasupstart13 <nicolas@upstart13.com>
2020-10-06 01:06:19 -0500

    (Style) Hide nav bar

--- app/src/main/java/com/upstart13/legba/HostActivity.java
+++ app/src/main/java/com/upstart13/legba/HostActivity.java
@@ -60,4 +60,18 @@ public class HostActivity extends AppCompatActivity {
         NavigationUI.setupWithNavController(toolbar, navController, appBarConfiguration);
     }
 
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            hideSystemUI();
+        }
+    }
+
+    private void hideSystemUI() {
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        decorView.setSystemUiVisibility(uiOptions);
+    }
+
 }
